5a5ff56ae5ebd7c99a880a28f769305349ccfaed
Roman Mazzei <mazzeiroman@gmail.com>
2020-10-05 02:03:29 -0400

    added todays information to the page, calling the correspondant http API

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var APIKey = "e17e89ad29a637df3767ab03ebde55a4";
+// var city = "";
+// var queryURL ="http://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + APIKey;
+
+// console.log(queryURL)
 var fiveDays = $(".fiveDays")
  fiveDays.addClass("row");
 
@@ -19,4 +24,50 @@ newDivCol2.append("<p>" +"Temp"+ "</p>");
 var newDivCol11 =  $("<div>" + "</div>");
 newDivCol11.addClass("col-md-1");
 fiveDays.append(newDivCol11);
+
+$(document).on('click', '.searchBtn', function(event) {
+    event.preventDefault();  
+  
+   var city = $("input").val();
+    var queryURL ="http://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + APIKey; 
+
+    var newDivRow = $("<div>" +city +"</div>");
+    newDivRow.addClass("row alert alert-warning")
+    $(".col-md-4").append(newDivRow);
+  
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+
+      // Log the queryURL
+      console.log(queryURL);
+
+      // Log the resulting object
+      console.log(response);
+      
+      // Convert the temp to fahrenheit
+      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+      // add temp content to html
+      $(".temp").text("Temperature (K) " + response.main.temp);
+      $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+
+      // Transfer content to HTML
+    $(".displayC").text(city);
+    $("#temp").text("Temperature: " + tempF+" ℉ ");
+    $("#hum").text("Humidity: " + response.main.humidity + " %");
+    $("#wind").text("Wind Speed: " + response.wind.speed+ " MPH");
+
+      // Log the data in the console as well
+      console.log("Wind Speed: " + response.wind.speed);
+      console.log("Humidity: " + response.main.humidity);
+      console.log("Temperature (F): " + tempF);
+    })
+
+})
 
